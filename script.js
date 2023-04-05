c04f88a44def14144ab30ed892c0730f67ed98c8
rihard-cyber <richardpl.meha@gmail.com>
2023-04-05 16:48:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
+//player1
 var chance = 3;
 alert("tebak angka 1-10" + "\n kamu punya " + chance + " kesempatan");
 chance--;
 while (chance >= 0) {
-  var user = prompt("masukan angka tebakan");
+  var player1 = prompt("player 1, Silakan masukan angka tebakan");
   var compt = Math.floor(Math.random() * 11);
 
-  if (user == compt) {
-    alert("TEBAKAN ANDA BENAR" + "\n ANGKA YANG DICARI " + compt);
+  if (player1 == compt) {
+    alert("TEBAKAN ANDA BENAR" + "\n ANGKA YANG DICARI" + compt);
     break;
   } else if (chance == 0) {
-    alert("kesempatan anda telah habis ");
+    alert("kesempatan anda telah habis giliran player dua");
     break;
-  } else if (user > compt) {
+  } else if (player1 > compt) {
     alert(
-      "TEBAKAN ANDA TERLALU TINGGI" + "\n kamu punya " + chance + " kesempatan"
+      "TEBAKAN ANDA TERLALU TINGGI" +
+        "\n Player 1 kamu punya" +
+        chance +
+        " kesempatan"
     );
-  } else if (user < compt) {
+  } else if (player1 < compt) {
     alert(
-      "TEBAKAN ANDA TERLALU RENDAH" + "\n kamu punya " + chance + " kesempatan"
+      "TEBAKAN ANDA TERLALU RENDAH" +
+        "\n Player 1 kamu punya" +
+        chance +
+        " kesempatan"
+    );
+    chance--;
+  }
+}
+//player2
+var chance = 3;
+alert("tebak angka 1-10" + "\n kamu punya " + chance + " kesempatan");
+chance--;
+while (chance >= 0) {
+  var player2 = prompt("player 2,Silakan masukan angka tebakan");
+  var compt = Math.floor(Math.random() * 11);
+
+  if (player2 == compt) {
+    alert("TEBAKAN ANDA BENAR" + "\n ANGKA YANG DICARI" + compt);
+    break;
+  } else if (chance == 0) {
+    alert("kesempatan anda telah habis");
+    break;
+  } else if (player2 > compt) {
+    alert(
+      "TEBAKAN ANDA TERLALU TINGGI" +
+        "\n Player 2 kamu punya" +
+        chance +
+        " kesempatan"
+    );
+  } else if (player2 < compt) {
+    alert(
+      "TEBAKAN ANDA TERLALU RENDAH" +
+        "\n Player 2 kamu punya" +
+        chance +
+        " kesempatan"
     );
     chance--;
   }
